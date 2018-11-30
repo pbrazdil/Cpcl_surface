6edd2464a81ee9bf01c87d36d221df0549ab4826
Petr Brazdil <p.brazdil@gmail.com>
2018-11-30 17:36:33 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Cpcl_surface",
-    pkgConfig: "pcl_surface-1.8",
+    pkgConfig: "pcl_surface-1.9",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
